d228c0591e5b2a27618e5af4d18018e4b7fb6599
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-26 21:48:48 +0100

    App.jsx routing setup & index.css styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,14 @@
+@type {import('tailwindcss').Config}
 module.exports = {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
-};
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
