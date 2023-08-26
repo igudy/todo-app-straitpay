3b9ae43e830d6da3a31ebce9ff0d5d07802db313
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-26 05:49:23 +0100

    Model added

---
+++ server/models/Task.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Must provide name"],
+        trim: true,
+        maxLength: [1000, "Name can not exceed 1000 characters"]
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    }
+})
+
+const Task = mongoose.model('Task', TaskSchema)
+
+export default Task;
