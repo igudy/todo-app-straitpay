5b404c2c7159d5211c29c68c388f0f228128f236
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-08-27 14:09:43 +0100

    Footer fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import Todo from "./components/Todo";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EditTask from './components/Edit'
 import './index.css'
+import Footer from "./components/Footer";
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 					<Route path="/edit/:taskId" element={<EditTask />} />
 				</Routes>
 			</Router>
+			<Footer />
 		</>
 	);
 }
